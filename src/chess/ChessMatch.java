23612c4b12854e6fde073603df43f3976325aa0d
Matheus <matheus.28fbr@gmail.com>
2020-02-14 15:56:52 -0300

    Chess layer ad printing the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -10,7 +10,7 @@ public class ChessMatch {
 		board = new Board(8,8 );
 	}
 	
-	public ChessPiece[][] GetPieces() {
+	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for(int i=0; i<board.getRows(); i++) {
 			for(int j=0; j<board.getColumns(); j++) {
