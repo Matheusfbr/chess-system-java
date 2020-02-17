ccf8f047836d6314f007b8d8562063015632d381
Matheus <matheus.28fbr@gmail.com>
2020-02-17 13:20:04 -0300

    melhorando a impressão

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,12 @@
 package application;
 
-import java.util.Scanner;
 import chess.ChessMatch;
-
 
 public class Program {
 
 	public static void main(String[] args) {
-
+		
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
-		
-		
-		
-;
 	}
-
 }
